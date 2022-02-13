ab9e711bbb44dbf5a5c0e7e982dacfa5f7c74db2
Karen200088 <k.h.a.200088@gmail.com>
2022-02-13 22:57:32 +0400

    eslint configs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-    return (
-        <p>aaa</p>
-
-    );
-
+  return (
+    <div>fdsdfd</div>
+  );
 }
 
 export default App;
