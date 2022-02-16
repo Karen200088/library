3e88658e0bd7438368db4ed206098056e04b82ab
Karen200088 <k.h.a.200088@gmail.com>
2022-02-16 21:51:00 +0400

    start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
-    <div>Npm run storybook</div>
+    <div>npm run storybook</div>
   );
 }
 
